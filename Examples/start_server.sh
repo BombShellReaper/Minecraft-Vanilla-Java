69744647a982e87ad5a3e4e87fce20935bb9738c
BombShellReaper <165164911+BombShellReaper@users.noreply.github.com>
2025-01-06 10:59:21 -0500

    Update start_server.sh

--- Examples/start_server.sh
+++ Examples/start_server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Variables
-SERVER_DIR="$HOME/server_dir_name"           # Change this to the directory you created earlier
-MEMORY_SETTINGS="-Xmx1024M -Xms1024M"        # Adjust based on your server's requirements
+SERVER_DIR="$HOME/server_dir_name"    # Replace "server_dir_name" with the directory you made earlier.
+MEMORY_SETTINGS="-Xmx3G -Xms1G"       # Rpelace "3G" & "1G" with what ever you want the max and min RAM set to.
+SCREEN_NAME="Minecraft_Server"        # Replace "Minecrafy_Server" with whatever you'd like.
 
 # Start the server in a detached screen session
-screen -dmS Minecraft_Server bash -c "cd \"$SERVER_DIR\" && java $MEMORY_SETTINGS -jar server.jar nogui"
+screen -dmS ${SCREEN_NAME} bash -c "cd ${SERVER_DIR} && java ${MEMORY_SETTINGS} -jar server.jar nogui"
 
 echo "Minecraft server started in screen session 'Minecraft_Server'."
